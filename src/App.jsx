2afaa2b16947b0f948aaaebc5b5bd53a8fc91a92
Ayush <bhandariayush935@gmail.com>
2025-09-03 00:53:35 +0530

    router enabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import { useState } from 'react'
 import './App.css'
+import Dashboard from './components/Dashboard'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Login from './components/Login'
+import { createBrowserRouter , RouterProvider } from 'react-router-dom'
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <><Navbar /><Home /></>,
+    },
+    {
+      path: '/dashboard',
+      element: <><Navbar /><Dashboard /></>,
+    },
+    {
+      path: '/login',
+      element: <><Navbar /><Login /></>,
+    },
+  ]);
+
   return (
     <>
-    
+      <RouterProvider router={router} />
     </>
   )
 }
